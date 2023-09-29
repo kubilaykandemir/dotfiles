0c293c781b04206206df84c64620c1b5b0869b97
Resat Kubilay Kandemir <kubilaykandemir@outlook.com>
2023-09-29 20:37:58 +1000

    Added scrollbar and gitsigns integration with scrollbar, changed bufferline to slanted tabs, change cattpuccin.lua to colorscheme.lua

--- nvim/.config/nvim/lua/kubi/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/kubi/plugins/gitsigns.lua
@@ -1,25 +1,30 @@
 return {
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
+  dependencies = {
+    "petertriho/nvim-scrollbar",
+  },
   -- See `:help gitsigns.txt`
   config = function()
+    require("scrollbar.handlers.gitsigns").setup()
     require('gitsigns').setup {
 
-  -- See `:help gitsigns.txt`
-  signs = {
-    add = { text = '+' },
-    change = { text = '~' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-  },
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
 
-  on_attach = function(bufnr)
-    vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-    vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-    vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
+          { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+      end
+
+    }
   end
-
-}
-end
 }
